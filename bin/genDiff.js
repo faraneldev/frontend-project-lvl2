48708108308ecd6ef7782d973c3fecf5e3a97e9e
faranel <faraneldev@gmail.com>
2021-11-23 13:24:26 +0300

    done 2 task

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -6,6 +6,8 @@ import genDiff from '../src/index.js';
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
+  .arguments('<filepath1> <filepath2>')
+  .version('0.1.0')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
